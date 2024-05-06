c27c72b3e55861d4c3f0edbc244e2af709ce2c51
Yowandaradillaa <yowandaradilla13@gmail.com>
2024-05-06 11:42:11 +0700

    memasukan function main

--- Friendfuction2.cpp
+++ Friendfuction2.cpp
@@ -22,3 +22,11 @@ void manusia::showNilaiPelajar(pelajar &x)
 {
     cout << x.nilai;
 }
+
+int main ()
+{
+    manusia budi;
+    pelajar pBudi;
+    budi.showNilaiPelajar(pBudi);
+    return 0;
+}
